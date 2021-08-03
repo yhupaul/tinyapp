346fc49daae23ea9f31626253d3675f62cfd7925
yhupaul <yhupaul@hotmail.com>
2021-08-02 17:33:31 -0700

    connecting with views

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
