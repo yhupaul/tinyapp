1311fe3aa43c1991541430f4c0b47625ff8aeba5
yhupaul <yhupaul@hotmail.com>
2021-08-02 17:46:37 -0700

    add short urls

--- express_server.js
+++ express_server.js
@@ -27,6 +27,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: href='#http://www.lighthouselabs.ca' };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
